1f8adbf0e8c2340ae68b3ccea15c88c9bc7334d2
augustoramirez46 <tutoead46@gmail.com>
2020-05-13 03:38:37 -0500

    ya agarra el id de cada producto

--- routes.js
+++ routes.js
@@ -1,20 +1,39 @@
 const assert = require('assert');
+const ObjectId = require('mongodb').ObjectId;
 
 function configureRoutes(app, db) {
 
-    // importar lista de productos
-    const products = require('./products');
 
-
-    // recorrer productos para agregar free shipping
-    products.forEach(function (elem) {
-
-        if (elem.price >= 80000) {
-            elem.freeShipping = true;
-        } else {
-            elem.freeShipping = false;
+    app.get('/producto/:name/:id', function (req, res) {
+        if (req.params.id.length != 24) {
+            res.redirect('/404');
+            return;
         }
+
+        const filter = {
+            _id: {
+                $eq: new ObjectId(req.params.id)
+            }
+        };
+        // Get the documents collection
+        const collection = db.collection('products');
+        // Find some documents
+        collection.find(filter).toArray(function (err, docs) {
+            assert.equal(err, null);
+
+            if (docs.length == 0) {
+                res.redirect('/404');
+                return;
+            }
+
+            // crear el contexto
+            var context = docs[0];
+            // renderizar el archivo list.handlebars con el contexto creado
+            res.render('product', context);
+        });
     });
+
+
 
     //todas las PREGUNTAS tienen que tener RESPUESTAS
     app.get('/', function (req, res) {
@@ -119,47 +138,28 @@ function configureRoutes(app, db) {
     })
 
     // el despues del : es la variable de nombre que recibe el handlebar
-    app.get('/producto/:name/:id', function (req, res) {
 
-        var id = parseInt(req.params.id);
-        var product = products[id];
+    // app.get('/producto/:name/:id', function (req, res) {
+
+    //     var id = parseInt(req.params.id);
+    //     var product = products[id];
 
 
-        var context = {};
+    //     var context = {};
 
-        // encontrar elemento en la lista
-        var foundElement = products.find(function (elem) {
-            if (elem.id == req.params.id) {
-                return true;
-            }
-        });
+    //     // encontrar elemento en la lista
+    //     var foundElement = products.find(function (elem) {
+    //         if (elem.id == req.params.id) {
+    //             return true;
+    //         }
+    //     });
 
-        context = foundElement;
-        /*
-            if (req.params.name === 'pantalon') {
-                var context = {
-                    title: '',
-                    img: '',
-                    description: '',
-                    options: ['rojo', 'verde'],
-                }
-            };
+    //     context = foundElement;
 
-            if (req.params.name === 'camiseta') {
-                var context = {
-                    title: '',
-                    img: '',
-                    description: '',
-                    options: [],
-                }
-            };
+    //     res.render('product', product);
 
-            console.log(req.params.name);
-            */
+    // });
 
-        res.render('product', product /*context*/ );
-
-    });
 }
 
 module.exports = configureRoutes;
